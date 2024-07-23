d504024e83cff7a923d762ff38c4f31bbccfe00e
Faustina F. Honsiah <fhonsiah@gmail.com>
2024-07-23 13:47:15 +0300

    creating a new case for rendering the count of the fields from the table name GENDER_EQUALITY

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/NavigationInteractor.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/NavigationInteractor.java
@@ -581,6 +581,14 @@ public class NavigationInteractor implements NavigationContract.Interactor {
                                 "   from " + org.smartregister.chw.lab.util.Constants.TABLES.LAB_TEST_REQUESTS + " p " +
                                 "              where p.patient_id is not null and p.results is not null and p.date_results_provided_to_client is null and p.is_closed is 0 ";
                 return NavigationDao.getQueryCount(sqlLab);
+
+                //here we create a new case for rendering the count of the fields from the model or table
+            case CoreConstants.TABLE_NAME.GENDER_EQUALITY:
+                String sqlGe =
+                        "SELECT count(*) " +
+                                "   from " + CoreConstants.TABLE_NAME.GENDER_EQUALITY+ " ege " +
+                                "   where ege.is_closed is 0 ";
+                return NavigationDao.getQueryCount(sqlGe);
             default:
                 return NavigationDao.getTableCount(tableName);
         }
